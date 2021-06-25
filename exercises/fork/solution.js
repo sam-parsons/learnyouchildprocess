27cf446b9ae754bb9e667e1021abb1fe0cdc7347
Sam Parsons <samparsons269@gmail.com>
2021-06-25 16:22:58 -0400

    refactored and cleaned up solution files for fork, spawn, spawn-stream

--- exercises/fork/solution.js
+++ exercises/fork/solution.js
@@ -1,6 +1,7 @@
 const { fork } = require('child_process');
+
 const forkedProcess = fork(process.argv[2]);
 
-forkedProcess.on('message', (m) => {
-  console.log(m);
+forkedProcess.on('message', (msg) => {
+  console.log(msg);
 });
--- exercises/spawnstream/solution.js
+++ exercises/spawnstream/solution.js
@@ -1,6 +1,6 @@
 const { spawn } = require('child_process');
-const cat = spawn('cat', [process.argv[2]]);
-const sort = spawn('sort');
+const catProcess = spawn('cat', [process.argv[2]]);
+const sortProcess = spawn('sort');
 
-cat.stdout.pipe(sort.stdin);
-sort.stdout.pipe(process.stdout);
+catProcess.stdout.pipe(sortProcess.stdin);
+sortProcess.stdout.pipe(process.stdout);
